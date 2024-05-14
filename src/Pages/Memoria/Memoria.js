2b8e5ae518ebeeb4b58d1767581806046ebb0628
Andres Muniz Piniella <amp-git@c4ad.eu>
2024-05-14 14:22:08 +0100

    memory1.5 limpiando

--- src/Pages/Memoria/Memoria.js
+++ src/Pages/Memoria/Memoria.js
@@ -5,6 +5,7 @@
 // Para el funcionamiento del juego tenemos que tener en cuenta que cuando hagamos click en una carta esta se dará la vuelta, controlaremos cuando el usuario hace el segundo click para levantar una segunda, se compararán y si son iguales se quedarán levantadas y ganaremos un punto, si no coinciden se volverán a dar la vuelta. https://www.w3schools.com/howto/howto_css_flip_card.asp
 
 import { Card } from '../../components/Card/Card'
+import { shuffleArray } from '../../components/shuffle/shuffle'
 import './Memoria.css'
 import listaImages from './MemoriaData'
 
@@ -19,6 +20,7 @@ export const Memoria = () => {
   divApp.appendChild(gameElement)
   gameElement.classList.add('gridCards', 'green', 'tapete')
 
+  shuffleArray(listaImages)
   listaImages.forEach((element) => {
     const cardA = document.createElement('a')
     gameElement.appendChild(cardA)
@@ -28,7 +30,7 @@ export const Memoria = () => {
 
     cardA.addEventListener(
       'click',
-      function voltea(_event) {
+      function voltea(event) {
         cardA.querySelector('figcaption').classList.toggle('hideCaption')
 
         cardA.querySelector('img').classList.toggle('hideImage')
@@ -37,40 +39,30 @@ export const Memoria = () => {
 
         console.log(elementsFlipped)
         if (elementsFlipped.length > 1) {
-          // console.log(
-          //   `primer elemento ${elementsFlipped[0].innerHTML} y segundo elemento ${elementsFlipped[1].innerHTML}`
-          // )
-          let elementFlipped01 = elementsFlipped[0]
-          let elementFlipped02 = elementsFlipped[1]
           if (
-            elementFlipped01.querySelector('h3').innerHTML ===
-            elementFlipped02.querySelector('h3').innerHTML
+            elementsFlipped[0].querySelector('h3').innerHTML ===
+            elementsFlipped[1].querySelector('h3').innerHTML
           ) {
             console.log(elementsFlipped[0].querySelector('h3').innerHTML)
             console.log(elementsFlipped[1].querySelector('h3').innerHTML)
             console.log('son iguales!')
-            // cardA.classList.toggle('flipped')
-            elementFlipped01.classList.add('paired')
-            elementFlipped02.classList.add('paired')
+            // elementsFlipped[0].classList.add('paired')
+            // elementsFlipped[1].classList.add('paired')
             //TODO creando una nueva colleccion aun solo quita al ultimo elemento
             //?https://developer.mozilla.org/es/docs/Web/API/EventTarget/removeEventListener#coincidiendo_disparadores_de_evento_para_su_eliminaci%C3%B3n
-            // var emparejadas = document.getElementsByClassName('paired')
-            // for (const carta of emparejadas) {
-            //   carta.removeEventListener('click', voltea, false)
-            // }
-            //TODO con esto quita el segundo pero no el primero. Si lo pongo al reves hace al reves
-            // elementFlipped02.removeEventListener('click', voltea)
-            // elementFlipped01.removeEventListener('click', voltea)
+            //! funciona por que le di la clase Paired
+            document.querySelectorAll('.flipped').forEach(function (el) {
+              //?no se porque el primer evento no se quita
+              el.removeEventListener('click', voltea, false)
+              el.classList.toggle('flipped')
+              el.classList.add('paired')
+            })
             //TODO esto lo saque de aqui pero lo que hace es copiar el elemento sin el evento: https://stackoverflow.com/questions/4386300/javascript-dom-how-to-remove-all-event-listeners-of-a-dom-object PERO FUNCIONA!
             // elementFlipped01.outerHTML = elementFlipped01.outerHTML
             // elementFlipped02.outerHTML = elementFlipped02.outerHTML
-            elementFlipped01.classList.toggle('flipped')
-            elementFlipped02.classList.toggle('flipped')
-            elementFlipped01.replaceWith(elementFlipped01.cloneNode(true))
-            elementFlipped02.replaceWith(elementFlipped02.cloneNode(true))
-
-            //TODO con esto hace solo el ultimo.
-            //cardA.removeEventListener('click', voltea)
+            //! o esto que parece que es mejor:
+            // elementFlipped01.replaceWith(elementFlipped01.cloneNode(true))
+            // elementFlipped02.replaceWith(elementFlipped02.cloneNode(true))
           } else {
             console.log('son distintos')
 
