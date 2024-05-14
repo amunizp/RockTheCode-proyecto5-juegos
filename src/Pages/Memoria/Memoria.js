1b7ac4a2e1de2682af9dddc70daaa5b0db3b6618
Andres Muniz Piniella <amp-git@c4ad.eu>
2024-05-14 13:36:45 +0100

    memory1.0

--- src/Pages/Memoria/Memoria.js
+++ src/Pages/Memoria/Memoria.js
@@ -23,73 +23,67 @@ export const Memoria = () => {
     const cardA = document.createElement('a')
     gameElement.appendChild(cardA)
     Card(cardA, element.location, element.letra, '', '', '50px', '50px')
-    const saveImage = cardA.firstChild.querySelector('img')
-    const saveCaption = cardA.firstChild.querySelector('figcaption')
-    saveCaption.classList.toggle('hideCaption')
-    cardA.addEventListener('click', function voltea(_event) {
-      saveCaption.classList.toggle('hideCaption')
-      saveImage.classList.toggle('hideImage')
-      cardA.classList.toggle('flipped')
-      var elementsFlipped = document.getElementsByClassName('flipped')
-      console.log(elementsFlipped.length)
-      console.log(elementsFlipped)
-      if (elementsFlipped.length > 1) {
-        // console.log(
-        //   `primer elemento ${elementsFlipped[0].innerHTML} y segundo elemento ${elementsFlipped[1].innerHTML}`
-        // )
-        let elementFlipped01 = elementsFlipped[0]
-        let elementFlipped02 = elementsFlipped[1]
-        if (
-          elementFlipped01.querySelector('h3').innerHTML ===
-          elementFlipped02.querySelector('h3').innerHTML
-        ) {
-          console.log(elementsFlipped[0].querySelector('h3').innerHTML)
-          console.log(elementsFlipped[1].querySelector('h3').innerHTML)
-          console.log('son iguales!')
-          // cardA.classList.toggle('flipped')
-          elementFlipped01.classList.add('paired')
-          elementFlipped02.classList.add('paired')
-          //TODO creando una nueva colleccion aun solo quita al ultimo elemento
+    // const saveImage = cardA.firstChild.querySelector('img')
+    cardA.querySelector('figcaption').classList.toggle('hideCaption')
 
-          // var emparejadas = document.getElementsByClassName('paired')
-          // for (const carta of emparejadas) {
-          //   carta.removeEventListener('click', voltea)
-          // }
-          //TODO con esto quita el segundo pero no el primero. Si lo pongo al reves hace al reves
-          // elementFlipped02.removeEventListener('click', voltea)
-          // elementFlipped01.removeEventListener('click', voltea)
-          //TODO esto lo saque de aqui pero lo que hace es copiar el elemento sin el evento: https://stackoverflow.com/questions/4386300/javascript-dom-how-to-remove-all-event-listeners-of-a-dom-object PERO FUNCIONA!
-          // elementFlipped01.outerHTML = elementFlipped01.outerHTML
-          // elementFlipped02.outerHTML = elementFlipped02.outerHTML
-          elementFlipped01.replaceWith(elementFlipped01.cloneNode(true))
-          elementFlipped02.replaceWith(elementFlipped02.cloneNode(true))
+    cardA.addEventListener(
+      'click',
+      function voltea(_event) {
+        cardA.querySelector('figcaption').classList.toggle('hideCaption')
 
-          console.log(elementsFlipped[0].innerHTML)
-          //TODO con esto hace solo el ultimo.
-          //cardA.removeEventListener('click', voltea)
+        cardA.querySelector('img').classList.toggle('hideImage')
+        cardA.classList.toggle('flipped')
+        var elementsFlipped = document.getElementsByClassName('flipped')
 
-          // cardA.removeEventListener('click', voltea)
-        } else {
-          console.log('son distintos')
-          saveCaption.classList.toggle('hideCaption')
-          saveImage.classList.toggle('hideImage')
-          cardA.classList.toggle('flipped')
-          console.log(
-            elementsFlipped[0].closest('figure').querySelector('figcaption')
-          )
-          elementsFlipped[0]
-            .closest('figure')
-            .querySelector('figcaption')
-            .classList.toggle('hideCaption')
-          console.log(elementsFlipped[0].closest('figure').querySelector('img'))
-          elementsFlipped[0]
-            .closest('figure')
-            .querySelector('img')
-            .classList.toggle('hideImage')
-          var flippedCard = document.querySelector('.flipped')
-          flippedCard.classList.toggle('flipped')
+        console.log(elementsFlipped)
+        if (elementsFlipped.length > 1) {
+          // console.log(
+          //   `primer elemento ${elementsFlipped[0].innerHTML} y segundo elemento ${elementsFlipped[1].innerHTML}`
+          // )
+          let elementFlipped01 = elementsFlipped[0]
+          let elementFlipped02 = elementsFlipped[1]
+          if (
+            elementFlipped01.querySelector('h3').innerHTML ===
+            elementFlipped02.querySelector('h3').innerHTML
+          ) {
+            console.log(elementsFlipped[0].querySelector('h3').innerHTML)
+            console.log(elementsFlipped[1].querySelector('h3').innerHTML)
+            console.log('son iguales!')
+            // cardA.classList.toggle('flipped')
+            elementFlipped01.classList.add('paired')
+            elementFlipped02.classList.add('paired')
+            //TODO creando una nueva colleccion aun solo quita al ultimo elemento
+            //?https://developer.mozilla.org/es/docs/Web/API/EventTarget/removeEventListener#coincidiendo_disparadores_de_evento_para_su_eliminaci%C3%B3n
+            // var emparejadas = document.getElementsByClassName('paired')
+            // for (const carta of emparejadas) {
+            //   carta.removeEventListener('click', voltea, false)
+            // }
+            //TODO con esto quita el segundo pero no el primero. Si lo pongo al reves hace al reves
+            // elementFlipped02.removeEventListener('click', voltea)
+            // elementFlipped01.removeEventListener('click', voltea)
+            //TODO esto lo saque de aqui pero lo que hace es copiar el elemento sin el evento: https://stackoverflow.com/questions/4386300/javascript-dom-how-to-remove-all-event-listeners-of-a-dom-object PERO FUNCIONA!
+            // elementFlipped01.outerHTML = elementFlipped01.outerHTML
+            // elementFlipped02.outerHTML = elementFlipped02.outerHTML
+            elementFlipped01.classList.toggle('flipped')
+            elementFlipped02.classList.toggle('flipped')
+            elementFlipped01.replaceWith(elementFlipped01.cloneNode(true))
+            elementFlipped02.replaceWith(elementFlipped02.cloneNode(true))
+
+            //TODO con esto hace solo el ultimo.
+            //cardA.removeEventListener('click', voltea)
+          } else {
+            console.log('son distintos')
+
+            //TODO modern solution https://stackoverflow.com/questions/22270664/how-to-remove-a-class-from-elements-in-pure-javascript might be best?
+            document.querySelectorAll('.flipped').forEach(function (el) {
+              el.querySelector('figcaption').classList.toggle('hideCaption')
+              el.querySelector('img').classList.toggle('hideImage')
+              el.classList.toggle('flipped')
+            })
+          }
         }
-      }
-    })
+      },
+      { passive: true }
+    )
   })
 }
